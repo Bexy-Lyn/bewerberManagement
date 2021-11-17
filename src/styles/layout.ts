003a46e5adbe9cef738d1ec568e2fcb9b1c88b7a
Bexy_Lyn <bexy_lyn@outlook.de>
2021-11-17 16:06:47 +0100

    responsive layout started

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -2,11 +2,19 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: 10% repeat(3, 1fr) 10%;
+  grid-template-columns: 1rem repeat(3, 1fr) 1rem;
   grid-template-rows: 1fr auto auto auto 1fr;
   grid-template-areas: '. . . . .' '. h1 h1 h1 .' '. lists lists lists .' '. add . . .' '. . . . .';
   grid-gap: 2rem;
   min-height: 100vh;
+  
+  @media screen and (min-width: 768px){
+    grid-template-columns: 2rem repeat(3, 1fr) 2rem;
+  }
+  
+  @media screen and (min-width: 1200px){
+    grid-template-columns: 10% repeat(3, 1fr) 10%;
+  }
 `;
 
 export const Lists = styled.section`
